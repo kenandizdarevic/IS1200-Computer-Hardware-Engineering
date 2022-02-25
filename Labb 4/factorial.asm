6fcc36c2df3ddcb930b14ee2236fa49eb3fd4a3a
Kenan Dizdarevic <99963700+Kenooooooo@users.noreply.github.com>
2022-02-25 13:52:54 +0100

    Add files via upload

--- Labb 4/factorial.asm
+++ Labb 4/factorial.asm
@@ -1,15 +1,15 @@
 .text
 
-	addi 	$a0, $zero, 3	# the immediate is n
-	add	$v0, $v0, $a0	# set answer = n
-	addi	$a1, $a0, -2	# i == (n - 2)
-	addi	$at, $zero, 1	# at == 1
+	addi 	$a0, $zero, 4		# the immediate is n
+	add	$v0, $v0, $a0		# set answer = n
+	addi	$a1, $a0, -2		# i == (n - 2)
+	addi	$at, $zero, 1		# at == 1
 	
 	beq	$a0, $zero, zero	# if(n == 0) go to zero
 	beq	$a0, $at, done		# if(n == 1) go to done
 	
 loop:
-	beq	$a1, $zero, done		# if(i == 0) go to done	
+	beq	$a1, $zero, done	# if(i == 0) go to done	
 	addi 	$a1, $a1, -1		# i--
 	addi 	$a2, $a0, -2		# j == (n - 2)
 	addi	$a0, $a0, -1		# n--
@@ -18,7 +18,7 @@ loop:
 multiply:
 	beq	$a2, $zero, loop	# if(j == 0) go to the outer loop
 	add	$v0, $v0, $a3		# answer += sum
-	addi	$a2, $a2, -1		
+	addi	$a2, $a2, -1		# j--
 	
 	beq	$zero, $zero, multiply	# continue iterating in the nested loop
 done:
